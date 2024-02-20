1f9d68e7997029186cdc7b0206fa058176814b9e
aarzootaksande <aarzutaksande@gmail.com>
2024-02-20 16:32:53 +0530

    20th Feb 2024 Evening 4:32 pm

---
+++ lecture_exercise_material/function_import_lecture.py
@@ -0,0 +1,5 @@
+# function import : a specific function is imported from a module
+
+from random import randint
+
+print(randint(200, 400))
---
+++ lecture_exercise_material/universal_import_lecture.py
@@ -0,0 +1,6 @@
+# universal import is when you import every type of function from a module so that whenever you any function from
+# that module you do not to type modules name and period
+
+from random import *
+
+print(random())
