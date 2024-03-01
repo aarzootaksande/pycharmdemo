bfcea8bea52822b1e6174f6338ac98f2f5bd1e7d
aarzootaksande <aarzutaksande@gmail.com>
2024-03-01 16:39:51 +0530

    1st March 2024 Work being Committed no 2 to Branch Main

---
+++ lecture_exercise_material/format_lecture.py
@@ -0,0 +1,8 @@
+# .format()
+#
+name = input(" What is the job application's name? ")
+degree = input(" What did they major in at college? ")
+job = input(" What is their current occupation? ")
+experience = input(" How many years have they been working in their field? ")
+
+print("{} majored in {}, works as a {}, and has {} years of experience.".format(name, degree, job, experience))
