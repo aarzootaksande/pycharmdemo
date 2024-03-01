872b4e81fb989e3d0cc08e66dd833968539ef6e5
aarzootaksande <aarzutaksande@gmail.com>
2024-03-01 15:29:44 +0530

    1st March 2024 Work being Committed to Branch Main

---
+++ lecture_exercise_material/lenght_lecture.py
@@ -0,0 +1,5 @@
+# len()  function gets lenght of iterative datatype such as strings
+print(len("trees are green"))
+print(len("This"+"is a"+"concatenated"+"string"))
+print("antidisestablishmentarianism"[7:20])
+print(len("antidisestablishmentarianism"[3:15]))
